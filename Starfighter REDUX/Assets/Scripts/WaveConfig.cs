9a135f02d2af5599e5aa260d582a6d3520b35e24
Julian McNeill <julesdowork@gmail.com>
2019-04-02 23:29:58 -0400

    WaveConfig Scriptable Object

---
+++ Starfighter REDUX/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Enemy Wave Configuration")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float spawnRate = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numEnemies = 10;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab() { return enemyPrefab; }
+
+    public GameObject GetPathPrefab() { return pathPrefab; }
+    
+    public float GetSpawnRate() { return spawnRate; }
+    
+    public float GetSpawnRandomFactor() { return spawnRandomFactor; }
+    
+    public int GetNumberOfEnemies() { return numEnemies; }
+    
+    public float GetMoveSpeed() { return moveSpeed; }
+
+
+}
